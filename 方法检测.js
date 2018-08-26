02480cb3be661030d8bceb68b782f68697db40ae
陈琪 <chenqi@chenqideMacBook-Pro.local>
2018-08-26 16:25:00 +0800

    fix bug for zero parameter

--- 方法检测.js
+++ 方法检测.js
@@ -1,31 +1,33 @@
 let obj = {
-    pickyMethodOne: function(obj, str, num) { /* ... */ },
-    pickyMethodTwo: function(num, obj) { /*... */ }
-  };
-  const argTypes = {
+    pickyMethodOne: function (obj, str, num) { /* ... */ },
+    pickyMethodTwo: function (num, obj) { /*... */ }
+};
+const argTypes = {
     pickyMethodOne: ["object", "string", "number"],
     pickyMethodTwo: ["number", "object"]
-  };
-  obj = new Proxy(obj, {
-    get: function(target, key, proxy) {
-      var value = target[key];
-      return function(...args) {
-          //console.log(args)
-        var checkArgs = argChecker(key, args, argTypes[key]);
-        return Reflect.apply(value, target, args);
-      };
+};
+obj = new Proxy(obj, {
+    get: function (target, key, proxy) {
+        var value = target[key];
+        return function (...args) {
+            //console.log(args)
+            var checkArgs = argChecker(key, args, argTypes[key]);
+            return Reflect.apply(value, target, args);
+        };
     }
-  });
-  
-  function argChecker(name, args, checkers) {
+});
+
+function argChecker(name, args, checkers) {
+    if (args.length < checkers.length) {
+        console.warn('parameters count incorrect.')
+    }
     for (var idx = 0; idx < args.length; idx++) {
-      var arg = args[idx];
-      var type = checkers[idx];
-      if (!arg || typeof arg !== type) {
-        console.warn(`You are incorrectly implementing the signature of ${name}. Check param ${idx + 1}`);
-      }
+        var arg = args[idx];
+        var type = checkers[idx];
+        if (!arg || typeof arg !== type) {
+            console.warn(`You are incorrectly implementing the signature of ${name}. Check param ${idx + 1}`);
+        }
     }
-  }
-  obj.pickyMethodOne();
-  obj.pickyMethodTwo("wopdopadoo", {});
-  
+}
+obj.pickyMethodOne();
+obj.pickyMethodTwo("wopdopadoo", {});
